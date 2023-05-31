f915c06c9e9f2a09559a238aef47488bb2901e1e
Pilici Rustam <paroli7328129ad@gmail.com>
2023-06-01 00:30:41 +0300

    [Стили] Верстаю дальше

--- src/features/MainPage/MainPage.jsx
+++ src/features/MainPage/MainPage.jsx
@@ -3,14 +3,25 @@ import styles from "./MainPage.module.css"
 import Filler from "./Filler";
 import logo from "./assets/logo-txt(x2).png"
 
-const MainPage = props => (
+const MainPage = () => (
     <div className={styles.mainDiv}>
         {/*<Filler/>*/}
-        <div className = {styles.rectangle41}>
-            <img className={styles.logoTxt} src={logo} alt=""/>
+        <div className={styles.welcomeDiv}>
+            <div className={styles.rectangle41}>
+                <div className={styles.rectangle16}/>
+                <img className={styles.logoTxt} src={logo} alt=""/>
+                <span className={styles.textBig1}>Становись</span>
+                <div className={styles.javaTextPosition}>
+
+                    <span className={styles.textBigJavaBrackets}>{"{"}</span>
+                    <span className={styles.textBigJava}>Java</span>
+                    <span className={styles.textBigJavaBrackets}>{"}"}</span>
+                    <span>-разработчиком</span>
+                </div>
+            </div>
+            <div className={styles.ellipse4}/>
+            <div className={styles.ellipse5}/>
         </div>
-        <div className={styles.ellipse4}/>
-        <div className={styles.ellipse5}/>
     </div>
 );
 
