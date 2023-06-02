5517db438fa0788254bc69e34d005a185df2eaf2
Pilici Rustam <paroli7328129ad@gmail.com>
2023-06-02 18:26:23 +0300

    [Стили] сменил absolute на relative

--- src/features/MainPage/MainPage.jsx
+++ src/features/MainPage/MainPage.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import styles from "./MainPage.module.css"
 import Filler from "./Filler";
 import logo from "./assets/logo-txt(x2).png"
+import Box from "@mui/material/Box";
 
 const MainPage = () => (
     <div className={styles.mainDiv}>
         {/*<Filler/>*/}
         <div className={styles.welcomeDiv}>
             <div className={styles.rectangle41}>
-                <div className={styles.rectangle16}/>
                 <img className={styles.logoTxt} src={logo} alt=""/>
-                <span className={styles.textBig1}>Становись</span>
-                <div className={styles.javaTextPosition}>
-
-                    <span className={styles.textBigJavaBrackets}>{"{"}</span>
-                    <span className={styles.textBigJava}>Java</span>
-                    <span className={styles.textBigJavaBrackets}>{"}"}</span>
-                    <span>-разработчиком</span>
+                <div className={styles.rectangle41TextBox}>
+                    <span className={styles.textBig1}>Становись</span>
+                    <div className={styles.javaTextPosition}>
+                        <div className={styles.rectangle16}/>
+                        <span className={styles.textBigJavaBrackets}>{"{"}</span>
+                        <span className={styles.textBigJava}>Java</span>
+                        <span className={styles.textBigJavaBrackets}>{"}"}</span>
+                        <span className={styles.textBig3}>-разработчиком</span>
+                    </div>
+                    <span className={styles.textBig2}>вместе с нами</span>
                 </div>
+                <div className={styles.ellipse5}/>
+                <div className={styles.ellipse4}/>
             </div>
-            <div className={styles.ellipse4}/>
-            <div className={styles.ellipse5}/>
         </div>
     </div>
 );
