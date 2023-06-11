2e854e41725bb99e69fd6196701a89fbaf4f925b
Pilici Rustam <paroli7328129ad@gmail.com>
2023-06-11 19:06:19 +0300

    [RESPONSIVE] TABlETS (768 - 1400) готово

--- src/features/MainPage/MainPage.jsx
+++ src/features/MainPage/MainPage.jsx
@@ -16,60 +16,57 @@ const MainPage = () => {
         <div className="mainDiv">
             {/*<Filler/>*/}
             <div className="welcomeDiv">
-                <div className="rectangle41">
-                    <img className="logoTxt" src={logo} alt=""/>
-                    <Fade in={clicked}>
-                        <div className="rectangle41TextBox">
-                            <span className="textBig1">Становись</span>
-                            <div className="javaTextPosition">
-                                <div className="rectangle16__above1400"/>
-                                <span className="textBigJavaBrackets">{"{"}</span>
-                                <span className="textBigJava">Java</span>
-                                <span className="textBigJavaBrackets">{"}"}</span>
-                                <span className="textBig3">-разработчиком</span>
-                            </div>
-                            <div className="rectangle16"/>
-                            <span className="textBig2">вместе с нами</span>
-                            <img className="group4" src={group4} alt=""/>
-                            <img className="group5" src={group5} alt=""/>
-                            <div className="group6_ButtonsContainer">
-                                <button className="group6_Button1">
-                                    <span className="group6_ButtonContent">Об обучении</span>
-                                </button>
-                                <button className="group6_Button2">
-                                    <span className="group6_ButtonContent">Преимущества</span>
-                                </button>
-                                <button className="group6_Button3">
-                                    <span className="group6_ButtonContent">Отзывы</span>
-                                </button>
-                                <button className="group6_Button4">
-                                    <span className="group6_ButtonContent">Ценовая политика</span>
-                                </button>
-                            </div>
-                            <div className="textSmall1_Container">
-                                <span className="textSmall1">
-                                Получи классную востребованную профессию и зарабатывай  дома
+                <div className="rectangle41"/>
+                <img className="logoTxt" src={logo} alt=""/>
+                <Fade in={clicked}>
+                    <div className="rectangle41TextBox">
+                        <span className="textBig1">Становись</span>
+                        <div className="javaTextPosition">
+                            <div className="rectangle16__above1400"/>
+                            <span className="textBigJavaBrackets">{"{"}</span>
+                            <span className="textBigJava">Java</span>
+                            <span className="textBigJavaBrackets">{"}"}</span>
+                            <span className="textBig3">-разработчиком</span>
+                        </div>
+                        <div className="rectangle16"/>
+                        <span className="textBig2">вместе с нами</span>
+                        <img className="group4" src={group4} alt=""/>
+                        <img className="group5" src={group5} alt=""/>
+                        <div className="group6_ButtonsContainer">
+                            <button className="group6_Button1">
+                                <span className="group6_ButtonContent">Об обучении</span>
+                            </button>
+                            <button className="group6_Button2">
+                                <span className="group6_ButtonContent">Преимущества</span>
+                            </button>
+                            <button className="group6_Button3">
+                                <span className="group6_ButtonContent">Отзывы</span>
+                            </button>
+                            <button className="group6_Button4">
+                                <span className="group6_ButtonContent">Ценовая политика</span>
+                            </button>
+                        </div>
+                        <div className="textSmall1_Container">
+                            <span className="textSmall1">
+                                Получи классную востребованную профессию и зарабатывай дома
                                 <br/>
                                 в удобное время
                                 </span>
-                            </div>
-                            <button onClick={handleClick} className="buttonStartLearning">Начать учиться</button>
-                            <div className="auth_ButtonsContainer">
-                                <button className="auth_Button1">
-                                    <span>Вход</span>
-                                </button>
-                                <button className="auth_Button2">
-                                    <span>Регистрация</span>
-                                </button>
-                            </div>
                         </div>
-                    </Fade>
-
-
-                    <div className="ellipse5"/>
-                    <div className="ellipse4"/>
-                    <div>
+                        <button onClick={handleClick} className="buttonStartLearning">Начать учиться</button>
+                        <div className="auth_ButtonsContainer">
+                            <button className="auth_Button1">
+                                <span>Вход</span>
+                            </button>
+                            <button className="auth_Button2">
+                                <span>Регистрация</span>
+                            </button>
+                        </div>
                     </div>
+                </Fade>
+                <div className="ellipse5"/>
+                <div className="ellipse4"/>
+                <div>
                 </div>
             </div>
         </div>
