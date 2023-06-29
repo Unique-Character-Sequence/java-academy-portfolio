e77291bc019a94b9dbcf87fd686e7b86250ec880
Pilici Rustam <paroli7328129ad@gmail.com>
2023-06-29 20:45:16 +0300

    Google Auth начинаем настройку

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import './features/counter/CounterPage.css';
 import CounterPage from "./features/counter/CounterPage";
 import {Route, Routes} from "react-router-dom";
 import MainPage from "./features/MainPage/MainPage";
+import { useEffect } from 'react';
 
 
-const App = () => (
-    <div>
-        <Routes>
-            <Route path="/counter" element={<CounterPage/>}/>
-            <Route path="/" element={<MainPage/>}/>
-        </Routes>
-    </div>
-);
+const App = () => {
+    useEffect(() => {
+        /*global google*/
+    }, [])
+    return (
+        <div>
+            <Routes>
+                <Route path="/counter" element={<CounterPage/>}/>
+                <Route path="/" element={<MainPage/>}/>
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
