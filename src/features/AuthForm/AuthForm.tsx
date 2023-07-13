3ae2c137d3acaa405b0a45bb9538f6f508bc724c
Pilici Rustam <paroli7328129ad@gmail.com>
2023-07-13 16:32:03 +0300

    [+ redux-persist] Сохранение state в local storage

--- src/features/AuthForm/AuthForm.tsx
+++ src/features/AuthForm/AuthForm.tsx
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from "formik";
+import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import { signInSchemaType, signInValues } from "./signInSchema";
 import { FieldWithValidation } from "../ModalPopup/FieldWithValidation";
